4a981d8bdc5d42912249b5a3eee810862beed5dc
moviles319@gmail.com <moviles319@gmail.com>
2026-03-25 21:27:06 -0500

    diseño de login terminado

--- app/src/main/java/pe/edu/idat/app_retrofit_login/retrofit/api/PatitasService.kt
+++ app/src/main/java/pe/edu/idat/app_retrofit_login/retrofit/api/PatitasService.kt
@@ -2,6 +2,7 @@ package pe.edu.idat.app_retrofit_login.retrofit.api
 
 import pe.edu.idat.app_retrofit_login.retrofit.request.LoginRequest
 import pe.edu.idat.app_retrofit_login.retrofit.response.LoginResponse
+import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -10,7 +11,7 @@ import retrofit2.http.Query
 interface PatitasService {
 
     @POST("login.php")
-    fun loginPatitas(@Body loginRequest: LoginRequest) : LoginResponse
+    fun loginPatitas(@Body loginRequest: LoginRequest) : Call<LoginResponse>
     //localhost:8080/libros?busqueda=dasdasdas&stock=10
     @GET("libros")
     fun getLibros(@Query("busqueda") busqueda:String,
